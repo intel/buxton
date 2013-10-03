8e392671b935ac18370f568a2e77992e31e7561a
Michael Ikey Doherty <michael.i.doherty@intel.com>
2013-10-03 09:37:02 +0100

    Add skeleton for memory backend
    Memory backend will be used as the primary test for further backend
    implementation within Buxton, and will cause various API changes to
    ensure the system is well integrated.
    
    The following commits will have to restructure the existing API to
    make backends unaware of explicit datatypes, migrating to set/get
    value methods, typed via struct/union keys.
    
    Signed-off-by: Michael Ikey Doherty <michael.i.doherty@intel.com>

---
+++ src/db/memory.c
@@ -0,0 +1,63 @@
+/*
+ * This file is part of buxton.
+ *
+ * Copyright (C) 2013 Intel Corporation
+ *
+ * buxton is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include "../shared/hashmap.h"
+#include "../shared/log.h"
+#include "../include/bt-daemon.h"
+#include "../include/bt-daemon-private.h"
+
+/**
+ * Memory Database Module
+ *
+ * Used for quick testing and debugging of Buxton, to ensure protocol
+ * and direct access are working as intended.
+ * Note this is not persistent.
+ */
+
+
+static Hashmap *_resources;
+
+static int set_string(const char *key, const char *value) {
+	return false;
+}
+
+static int get_string(const char *key, char **out_value) {
+	return false;
+}
+
+_bx_export_ void buxton_module_destroy(BuxtonBackend *backend) {
+	backend->set_string = NULL;
+	backend->get_string = NULL;
+
+}
+
+_bx_export_ int buxton_module_init(const char *resource, BuxtonBackend *backend) {
+	/* Point the struct methods back to our own */
+	backend->set_string = &set_string;
+	backend->get_string = &get_string;
+
+	return 0;
+}
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
