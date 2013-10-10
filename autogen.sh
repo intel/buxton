5bb7738b703908914e2d08f9ef2b0dc9d9729f83
Patrick McCarty <patrick.mccarty@linux.intel.com>
2013-10-10 14:23:43 -0700

    autogen: enable silent rules by default

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,8 @@ autoreconf --force --install --symlink --warnings=all
 args="\
 --sysconfdir=/etc \
 --localstatedir=/var \
---libdir=/usr/lib"
+--libdir=/usr/lib \
+--enable-silent-rules"
 
 ./configure CFLAGS='-g -O0' $args "$@"
 make clean
