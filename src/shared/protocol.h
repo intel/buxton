ff84c7628f5baf8f0aec838d6cb771dafa59b7cc
Michael Ikey Doherty <michael.i.doherty@intel.com>
2013-10-25 12:50:02 +0100

    Begin implementation of wire protocol
    We'll handle all protocol implementation within the new protocol part
    of the library. This required moving BuxtonDaemon into the header file
    so that we can use typedef'd pointers to internal functions, to ensure
    we do not expose these members publicly or export the symbols.
    
    Signed-off-by: Michael Ikey Doherty <michael.i.doherty@intel.com>

--- src/shared/protocol.h
+++ src/shared/protocol.h
@@ -20,7 +20,18 @@
     #include "config.h"
 #endif
 
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include "list.h"
 #include "bt-daemon.h"
+
+typedef struct client_list_item {
+	LIST_FIELDS(struct client_list_item, item); /**<List type */
+	int fd; /**<File descriptor of connected client */
+	struct ucred cred; /**<Credentials of connected client */
+	char *smack_label; /**<Smack label of connected client */
+	char data[256]; /**<Data buffer for the client */
+} client_list_item;
 
 /**
  * Buxton Status Codes
@@ -29,7 +40,32 @@ typedef enum BuxtonStatus {
 	BUXTON_STATUS_OK = 0, /**<Operation succeeded */
 	BUXTON_STATUS_FAILED /**<Operation failed */
 } BuxtonStatus;
- 
+
+typedef BuxtonData* (*daemon_value_func) (client_list_item *client,
+					   BuxtonData *list,
+					   BuxtonStatus *status);
+
+/**
+ * Global store of bt-daemon state
+ */
+typedef struct BuxtonDaemon {
+	size_t nfds_alloc;
+	size_t accepting_alloc;
+	nfds_t nfds;
+	bool *accepting;
+	struct pollfd *pollfds;
+	client_list_item *client_list;
+	BuxtonClient buxton;
+	daemon_value_func set_value;
+	daemon_value_func get_value;
+} BuxtonDaemon;
+
+/**
+ * Handle a message within bt-daemon
+ * @param client Current client
+ */
+void bt_daemon_handle_message(BuxtonDaemon *self, client_list_item *client);
+
 /*
  * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
  *
