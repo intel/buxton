9dc57563a06c2314f2073fccebd687ede8e7cf5b
Patrick McCarty <patrick.mccarty@linux.intel.com>
2013-10-29 19:22:34 -0700

    autogen: set prefix for configure, remove libdir
    'libdir' uses the value of 'prefix', so it shouldn't be hardcoded.

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ autoreconf --force --install --symlink --warnings=all
 args="\
 --sysconfdir=/etc \
 --localstatedir=/var \
---libdir=/usr/lib \
+--prefix=/usr \
 --enable-silent-rules"
 
 ./configure CFLAGS='-g -O0' $args "$@"
