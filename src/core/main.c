f7c27e5ec79a24eb56fa3eed104fb83abe65c305
Michael Leibowitz <michael.leibowitz@intel.com>
2013-11-11 11:32:24 -0800

    mitigate a potential buffer overflow
    Signed-off-by: Michael Leibowitz <michael.leibowitz@intel.com>

--- src/core/main.c
+++ src/core/main.c
@@ -97,6 +97,7 @@ int main(int argc, char *argv[])
 		memset(&sa, 0, sizeof(sa));
 		sa.un.sun_family = AF_UNIX;
 		strncpy(sa.un.sun_path, BUXTON_SOCKET, sizeof(sa.un.sun_path) - 1);
+		sa.un.sun_path[sizeof(sa.un.sun_path)-1] = 0;
 
 		ret = unlink(sa.un.sun_path);
 		if (ret == -1 && errno != ENOENT) {
