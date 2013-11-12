4e61b522c51e27a23afd4a26c65419f53996a80b
William Douglas <william.douglas@intel.com>
2013-11-11 22:02:05 -0800

    shared: Add missed deserialize message return conversion

--- src/shared/protocol.c
+++ src/shared/protocol.c
@@ -59,7 +59,7 @@ int buxton_wire_get_response(BuxtonClient *client, BuxtonControlMessage *msg,
 			continue;
 
 		count = buxton_deserialize_message(response, &r_msg, l, &r_list);
-		if (count < 0)
+		if (count == 0)
 			goto end;
 		if (r_msg != BUXTON_CONTROL_STATUS || r_list[0].type != INT) {
 			buxton_log("Critical error: Invalid response\n");
