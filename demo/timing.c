6afc23fb77a59c9f9e4a974d23d5681ba42bb223
Michael Ikey Doherty <michael.i.doherty@intel.com>
2013-11-13 14:44:42 +0000

    Add skeleton for demo programs
    We'll use demo programs to test and help implement new and existing
    features within buxton, they are not intended for general use.
    
    Signed-off-by: Michael Ikey Doherty <michael.i.doherty@intel.com>

---
+++ demo/timing.c
@@ -0,0 +1,20 @@
+/*
+ * This file is part of buxton.
+ *
+ * Copyright (C) 2013 Intel Corporation
+ *
+ * buxton is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include <stdlib.h>
+
+#include "bt-daemon.h"
+
+int main(int argc, char **argv)
+{
+
+	return EXIT_SUCCESS;
+}
