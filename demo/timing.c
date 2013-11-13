5ed63720d74d15f95640b2f22021f6886a458f5f
Michael Ikey Doherty <michael.i.doherty@intel.com>
2013-11-13 16:14:17 +0000

    demos: Add timing demo
    This demonstration simply sets the same key and value 10,000 times
    and reports the total and average times for this operation. Note
    that debug should be disabled before performing this test, because
    constantly printing to the terminal will drastically reduce performance
    and is an unfair indication of Buxton performance.
    
    Signed-off-by: Michael Ikey Doherty <michael.i.doherty@intel.com>

--- demo/timing.c
+++ demo/timing.c
@@ -10,11 +10,76 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #include "bt-daemon.h"
+#include "util.h"
+
+#define error(...) { printf(__VA_ARGS__); goto end; }
+
+typedef bool (*TestFunction) (BuxtonClient *client);
+
+bool timed_func(TestFunction func, BuxtonClient *client, unsigned long long *total)
+{
+	struct timespec tsi, tsf;
+	float elapsed;
+	bool ret;
+
+	clock_gettime(CLOCK_MONOTONIC, &tsi);
+	ret = func(client);
+	clock_gettime(CLOCK_MONOTONIC, &tsf);
+
+	elapsed = tsf.tv_nsec - tsi.tv_nsec;
+	elapsed += *total;
+
+	*total = elapsed;
+	return ret;
+}
+
+bool set_value_test(BuxtonClient *client)
+{
+	BuxtonString layer, key, value;
+	BuxtonData data;
+	bool ret;
+
+	layer = buxton_string_pack("base");
+	key = buxton_string_pack("ValueTestTest");
+	value = buxton_string_pack("SomeRandomString");
+	buxton_string_to_data(&value, &data);
+	data.label = buxton_string_pack("_");
+
+	ret = buxton_client_set_value(client, &layer, &key, &data);
+	return ret;
+}
 
 int main(int argc, char **argv)
 {
+	BuxtonClient client;
+	int i;
+	int ret = EXIT_FAILURE;
+	unsigned long long total;
+	long num_tests = 0;
+	float average;
 
-	return EXIT_SUCCESS;
+	if (!buxton_client_open(&client))
+		error("Unable to open BuxtonClient\n");
+
+	if (!set_value_test(&client))
+		error("Unable to set string\n");
+
+	/* 10,000 set operations using wire protocol */
+	num_tests = 10000;
+	for (i=0; i < num_tests; i++) {
+		if (!timed_func(set_value_test, &client, &total))
+			error("Unable to set string\n");
+	}
+	total /= 1000000;
+	average = ((float)total) / ((float)num_tests);
+	printf("set_value_test: %ld operations took %lldms; Average time: %lfms\n", num_tests, total, average);
+
+	ret = EXIT_SUCCESS;
+	buxton_client_close(&client);
+end:
+	return ret;
 }
