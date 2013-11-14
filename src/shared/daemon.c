71f973a3f7c6d9e6c0cf9e0509b14348b33cf554
Patrick McCarty <patrick.mccarty@linux.intel.com>
2013-11-13 21:26:09 -0800

    daemon: wrap some of the long lines

--- src/shared/daemon.c
+++ src/shared/daemon.c
@@ -173,15 +173,22 @@ BuxtonData *set_value(BuxtonDaemon *self, client_list_item *client, BuxtonData *
 			return NULL;
 		}
 
-		bool valid = buxton_client_get_value_for_layer(&(self->buxton), &layer.store.d_string, &key.store.d_string, data);
+		bool valid = buxton_client_get_value_for_layer(&(self->buxton),
+							       &layer.store.d_string,
+							       &key.store.d_string,
+							       data);
 
-		if (!valid && !buxton_check_smack_access(client->smack_label, &(value.label), ACCESS_WRITE)) {
+		if (!valid && !buxton_check_smack_access(client->smack_label,
+							 &(value.label),
+							 ACCESS_WRITE)) {
 			buxton_debug("Smack: not permitted to set new value\n");
 			free(data);
 			return NULL;
 		}
 
-		if (valid && !buxton_check_smack_access(client->smack_label, &(data->label), ACCESS_WRITE)) {
+		if (valid && !buxton_check_smack_access(client->smack_label,
+							&(data->label),
+							ACCESS_WRITE)) {
 			buxton_debug("Smack: not permitted to modify existing value\n");
 			free(data);
 			return NULL;
