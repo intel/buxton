78bbb651046672f70715ecb0bc8822e38caee76c
William Douglas <william.douglas@intel.com>
2013-11-13 18:58:43 -0800

    test: Add a test utilities file

---
+++ test/check_utils.c
@@ -0,0 +1,36 @@
+/*
+ * This file is part of buxton.
+ *
+ * Copyright (C) 2013 Intel Corporation
+ *
+ * buxton is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include <check.h>
+#include <sys/socket.h>
+
+static void setup_socket_pair(int *client, int *server)
+{
+	int socks[2];
+	fail_if(socketpair(AF_UNIX, SOCK_STREAM, 0, socks),
+		"socketpair: %m");
+
+	*client = socks[0];
+	*server = socks[1];
+}
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
