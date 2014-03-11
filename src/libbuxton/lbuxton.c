8d22ee68cc891d86243b683b4bfecebe5567de0c
William Douglas <william.douglas@intel.com>
2014-03-10 17:18:12 -0700

    libbuxton: Update buxton_unregister_notification to return an int

--- src/libbuxton/lbuxton.c
+++ src/libbuxton/lbuxton.c
@@ -183,37 +183,37 @@ int buxton_register_notification(BuxtonClient client,
 	return ret;
 }
 
-bool buxton_unregister_notification(BuxtonClient client,
-					   BuxtonKey key,
-					   BuxtonCallback callback,
-					   void *data,
-					   bool sync)
+int buxton_unregister_notification(BuxtonClient client,
+				   BuxtonKey key,
+				   BuxtonCallback callback,
+				   void *data,
+				   bool sync)
 {
 	bool r;
-	int ret;
+	int ret = 0;
 	_BuxtonKey *k = (_BuxtonKey *)key;
 
 	if (!k || !k->group.value || !k->name.value ||
 	    k->type <= BUXTON_TYPE_MIN || k->type >= BUXTON_TYPE_MAX) {
-		return false;
+		return EINVAL;
 	}
 
 	r = buxton_wire_unregister_notification((_BuxtonClient *)client, k,
 						callback, data);
 	if (!r) {
-		return false;
+		return -1;
 	}
 
 	if (sync) {
 		ret = buxton_wire_get_response(client);
 		if (ret <= 0) {
-			r = false;
+			ret = -1;
 		} else {
-			r = true;
+			ret = 0;
 		}
 	}
 
-	return r;
+	return ret;
 }
 
 bool buxton_set_value(BuxtonClient client,
